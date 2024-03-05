f3697d6593216b090e12c0bd9e762724db63e256
David Freitas <davidfreitas.dev@outlook.com>
2024-03-05 15:27:20 -0300

    add auth pages structure

--- mobile/src/router/index.js
+++ mobile/src/router/index.js
@@ -1,33 +1,38 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
-import Home from '../views/Home.vue';
-import Habit from '../views/Habit.vue';
-import New from '../views/New.vue';
-
-const routes = [
-  {
-    path: '/',
-    redirect: '/home'
-  },
-  {
-    path: '/home',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/habit/:date',
-    name: 'Habit',
-    component: Habit
-  },
-  {
-    path: '/new',
-    name: 'New',
-    component: New
-  }
-];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes: [
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: () => import('@/views/auth/Signup.vue'),
+    },
+    {
+      path: '/signin',
+      name: 'Signin',
+      component: () => import('@/views/auth/Signin.vue'),
+    },
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: () => import('@/views/Home.vue'),
+    },
+    {
+      path: '/habit/:date',
+      name: 'Habit',
+      component: () => import('@/views/Habit.vue'),
+    },
+    {
+      path: '/new',
+      name: 'New',
+      component: () => import('@/views/New.vue'),
+    }
+  ]
 });
 
 export default router;
---
+++ mobile/src/views/auth/Signup.vue
@@ -0,0 +1,15 @@
+<template>
+  <ion-page>
+    <ion-content :fullscreen="true">
+      Sign Up
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
