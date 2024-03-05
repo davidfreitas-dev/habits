91e2478551fad74d56b891d8de08089fb308fd3b
David Freitas <davidfreitas.dev@outlook.com>
2024-03-05 18:29:09 -0300

    add .env use

--- mobile/src/api/axios.js
+++ mobile/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { useSessionStore } from '@/stores/session';
 
-const BASE_URL = 'http://localhost:8000';
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const instance = axios.create({
   baseURL: BASE_URL,
