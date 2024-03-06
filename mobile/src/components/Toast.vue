5d7e543c2cadc668a69b285f2ac0423edede1dd1
David Freitas <davidfreitas.dev@outlook.com>
2024-03-06 15:01:24 -0300

    add vuelidate and toast refactor

--- mobile/src/components/Toast.vue
+++ mobile/src/components/Toast.vue
@@ -1,15 +1,16 @@
 <script setup>
 import { ref } from 'vue';
 import { IonToast } from '@ionic/vue';
+import { alertCircleOutline } from 'ionicons/icons';
 
 const isOpenRef = ref(false);
-const toastColor = ref('');
+const color = ref('');
 const message = ref(false);
 
-const setOpen = (state, color, msg) => {
+const setOpen = (state, type, msg) => {
   isOpenRef.value = state;
-  toastColor.value = color;
   message.value = msg;
+  color.value = type === 'error' ? 'danger' : 'success';
 };
 
 defineExpose({setOpen});
@@ -17,9 +18,10 @@ defineExpose({setOpen});
 
 <template>
   <ion-toast
+    :icon="alertCircleOutline"
     :is-open="isOpenRef"
-    :color="toastColor"
     :message="message"
+    :color="color"
     :duration="2000"
     @did-dismiss="setOpen(false)"
   />
