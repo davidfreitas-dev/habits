52bc0fd39b6247ce9f65824d8019c751fca2f25c
David Freitas <davidfreitas.dev@outlook.com>
2026-02-22 15:24:50 -0300

    feat: set timezone

--- api/config/bootstrap.php
+++ api/config/bootstrap.php
@@ -8,6 +8,7 @@ define('APP_ROOT', dirname(__DIR__));
 
 require APP_ROOT . '/vendor/autoload.php';
 
-// Load environment variables
 $dotenv = Dotenv::createImmutable(APP_ROOT);
 $dotenv->load();
+
+date_default_timezone_set($_ENV['APP_TIMEZONE']);
